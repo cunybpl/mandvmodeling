912535a2307122eaca67e351df855d8aa0cb26de
peterphung2043 <pphung000@citymail.cuny.edu>
2024-11-08 11:09:25 -0500

    Added unit tests for  #4

--- tests/test_parameter_models.py
+++ tests/test_parameter_models.py
@@ -4,14 +4,14 @@ from changepointmodel.core.pmodels import (
 )
 from changepointmodel.core.pmodels.coeffs_parser import TwoParameterCoefficientParser
 from mandvmodeling.core.pmodels import MandVParameterModelFunction
-
+import pytest
 
 def test_modelfunction():
     def f(X, y):
         return (X + y).squeeze()
 
     bound = (42,), (43,)
-    parmeter_model = TwoParameterModel()
+    parameter_model = TwoParameterModel()
     parser = TwoParameterCoefficientParser()
     initial_guesses = (42, 43)
 
@@ -20,13 +20,13 @@ def test_modelfunction():
         f=f,
         bounds=bound,
         initital_guesses=initial_guesses,
-        parameter_model=parmeter_model,
+        parameter_model=parameter_model,
         coefficients_parser=parser,
     )
     assert model.name == "mymodel"
     assert model.f == f
     assert model.bounds == bound
-    assert model.parameter_model == parmeter_model
+    assert model.parameter_model == parameter_model
     assert model.initial_guesses == initial_guesses
 
     # Since the default for `initial_guesses == None`, test this.
@@ -34,7 +34,7 @@ def test_modelfunction():
         "mymodel",
         f=f,
         bounds=bound,
-        parameter_model=parmeter_model,
+        parameter_model=parameter_model,
         coefficients_parser=parser,
     )
 
@@ -43,3 +43,61 @@ def test_modelfunction():
     assert model.parse_coeffs((42, 99)) == EnergyParameterModelCoefficients(
         42, [99], []
     )
+
+def test_modelfunction_validator():
+    def f(X, y):
+        return (X + y).squeeze()
+
+    bound = (42,), (43,)
+    # Don't use instances like in `test_modelfunction`, just use
+    # the classes themselves 
+    parameter_model = TwoParameterModel
+    parser = TwoParameterCoefficientParser
+    initial_guesses = (42, 43)
+
+    model = MandVParameterModelFunction(
+        "mymodel",
+        f=f,
+        bounds=bound,
+        initital_guesses=initial_guesses,
+        parameter_model=parameter_model,
+        coefficients_parser=parser,
+    )
+    assert model.name == "mymodel"
+    assert model.f == f
+    assert model.bounds == bound
+    assert model.parameter_model == parameter_model
+    assert model.initial_guesses == initial_guesses
+
+    # Use `parameter_model` class in the `coefficients_parser` param.
+    with pytest.raises(TypeError):
+        model = MandVParameterModelFunction(
+            "mymodel",
+            f=f,
+            bounds = bound,
+            initital_guesses=initial_guesses,
+            parameter_model=parameter_model,
+            coefficients_parser=parameter_model
+        )
+
+    # Use `parser` class in the `parameter_model` param
+    with pytest.raises(TypeError):
+        model = MandVParameterModelFunction(
+            "mymodel",
+            f=f,
+            bounds = bound,
+            initital_guesses=initial_guesses,
+            parameter_model=parser,
+            coefficients_parser=parser
+        )
+
+    with pytest.raises(TypeError):
+        # Use `parser` class in the `f` param
+        model = MandVParameterModelFunction(
+            "mymodel",
+                f=parser,
+                bounds = bound,
+                initital_guesses=initial_guesses,
+                parameter_model=parser,
+                coefficients_parser=parser
+        )
